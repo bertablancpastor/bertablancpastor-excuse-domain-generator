e451447c5f8fb35f431424661b91d5c8ccde6e7d
bertablancpastor <72865106+bertablancpastor@users.noreply.github.com>
2023-05-29 09:57:09 +0000

    domain name generator finalizad con visualización en el Dom

--- src/app.js
+++ src/app.js
@@ -5,7 +5,24 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onclick = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  var pronoun = ["the", "our"];
+  var adj = ["great", "big"];
+  var noun = ["jogger", "racoon"];
+  let ext = [".cat", ".com", ".net"];
+  let message = [];
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        for (let l = 0; l < ext.length; l++) {
+          console.log(pronoun[i] + adj[j] + noun[k] + ext[l]);
+          message.push(pronoun[i] + adj[j] + noun[k] + ext[l] + "</br>");
+        }
+      }
+    }
+  }
+
+  document.getElementById("domainNameGenerator").innerHTML = message;
 };
